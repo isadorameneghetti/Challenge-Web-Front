a623bc63ada2c9232931ed9739ddaf45a46c2b7b
MateussantosVSr3 <mateus20100webn@gmail.com>
2024-09-27 14:07:57 -0300

    pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import './About.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import Photocar from '../../assets/3-Formula-E-Gen3-review.webp';
+import logo1 from '../../assets/ABB-logo.webp';
+import logo2 from '../../assets/1200px-SABIC_Logo_RGB.webp';
+import logo3 from '../../assets/Aggreko-Logo-RGB-01.webp';
+import logo4 from '../../assets/Allianz-sponsor.webp';
+import logo5 from '../../assets/antofagasta-minerals-logo.webp';
+import logo6 from '../../assets/Bosch-Logo-1925.webp';
+import logo7 from '../../assets/DHL_Logo.svg.webp';
+import logo8 from '../../assets/Hackett_Sport_Logo_Black.webp';
+import logo9 from '../../assets/HK_2019_Logo-1-.webp';
+import logo10 from '../../assets/Ifosys-Logo.webp';
+import logo11 from '../../assets/Julius-baer-logo.webp';
+import logo12 from '../../assets/pif_colour.webp';
+import logo13 from '../../assets/Saudia-logo-new.webp';
+import logo14 from '../../assets/southwire-logo.webp';
+import logo15 from '../../assets/taittinger_logo.webp';
+import logo16 from '../../assets/Tata-Communications-Logo-Stacked-Blue-New.webp';
+import logo17 from '../../assets/WhatsApp-Image-2024-03-14-at-20.27.28.webp';
+import logo18 from '../../assets/FIA-Emblem-Lockup.png';
+
 
 
 function About() {
@@ -35,8 +58,8 @@ function About() {
                 <div className="row">
                   <div className="col-sm-12">
                     <div className="banner_taital">
-                      <h1 className="outstanding_text">Sustainable Mobility and Emission Reduction</h1>
-                      <h1 className="sub_text">"Electric Racing: Driving Towards a Greener Future"</h1>
+                      <h1 className="outstanding_text">Sustainable Mobility & Emissions</h1>
+                      <h1 className="sub_text">Racing Towards a Greener Future</h1>
                       <p className="there_text">
                         Formula E is a motorsport category focused on promoting sustainable mobility through 100% electric vehicles that produce no emissions during races. The category encourages innovations in battery technology and energy efficiency, with solutions that can be applied to passenger vehicles. Additionally, Formula E aims to achieve carbon neutrality in its operations, contributing to a cleaner and more sustainable future.
                       </p>
@@ -53,8 +76,8 @@ function About() {
                 <div className="row">
                   <div className="col-sm-12">
                     <div className="banner_taital">
-                      <h1 className="outstanding_text">Social Initiatives and Environmental Education</h1>
-                      <h1 className="sub_text">"Empowering Communities: Racing for a Sustainable Tomorrow"</h1>
+                      <h1 className="outstanding_text">Social Impact & Eco-Education</h1>
+                      <h1 className="sub_text">Racing for a Greener Tomorrow</h1>
                       <p className="there_text">
                         In addition to the races, Formula E implements social initiatives and educational programs to promote electric mobility and environmental awareness. It conducts community activities in each city where a race takes place, aiming to inspire young people to adopt sustainable practices. The category uses its visibility to address issues such as air pollution and climate change, positioning itself as a transformative force for a greener and more inclusive future.
                       </p>
@@ -69,10 +92,10 @@ function About() {
 
             {/* Carousel Controls */}
             <a className="carousel-control-prev" href="#main_slider" role="button" data-bs-slide="prev">
-              <i className="fa fa-angle-left"></i>
+              <i className="fa fa-angle-left"><FontAwesomeIcon icon={faAngleLeft} /></i>
             </a>
             <a className="carousel-control-next" href="#main_slider" role="button" data-bs-slide="next">
-              <i className="fa fa-angle-right"></i>
+              <i className="fa fa-angle-right"><FontAwesomeIcon icon={faAngleRight} /></i>
             </a>
           </div>
         </div>
@@ -96,9 +119,9 @@ function About() {
                 </div>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 centralize">
               <div className="about_img">
-                <img src="images/about-img.png" alt="About Us" />
+                <img src={Photocar} alt="Man driving a Formula E car" />
               </div>
             </div>
           </div>
@@ -106,8 +129,8 @@ function About() {
       </div>
 
       {/* Sponsors */}
-      <div className="bg-dark">
-            <main className="vw-100 vh-100 d-flex flex-row justify-content-center align-items-center">
+      <div>
+            <main className="vw-100 d-flex flex-row justify-content-center align-items-center">
                 <div
                 className="logos overflow-hidden py-5 px-0 position-relative"
                 style={{ backgroundColor: '#fff', whiteSpace: 'nowrap' }}
@@ -115,48 +138,76 @@ function About() {
                     <div className="logos-slide d-inline-block">
                         {/* Imagens */}
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359520-3a125798-774e-423b-b738-5f5e495d276d.png"
-                        alt="Etsy"
+                        src={logo1}
+                        alt="ABB"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359525-2779538f-35e0-4727-89fa-04e523624630.png"
-                        alt="Airbnb"
+                        src={logo2}
+                        alt="SABIC"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359530-00ed0fa5-9752-46df-bc07-4ab5dfe3dded.png"
-                        alt="Amazon"
+                        src={logo3}
+                        alt="Aggreko"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359550-db8cbcba-e2ed-48e1-81dc-c396c35893eb.png"
-                        alt="Target"
+                        src={logo4}
+                        alt="Allianz"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359532-4f2a2b1b-8eb7-4aaa-bda9-7d31df0d5cff.png"
-                        alt="Netflix"
+                        src={logo5}
+                        alt="Antofagasta Minerals"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359536-909db7c6-660e-495d-9568-c63bdf1fab5d.png"
-                        alt="Nasa"
+                        src={logo6}
+                        alt="Bosch"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359540-ba6f0359-617b-4e69-9b02-5137a6063738.png"
-                        alt="Shopify"
+                        src={logo7}
+                        alt="DHL"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359552-631b683f-859c-44e5-991f-23ff98110083.png"
-                        alt="Uber"
+                        src={logo8}
+                        alt="Hackett"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359544-ccf33abb-f245-4c29-8f79-997e3eb206ee.png"
-                        alt="Slack"
+                        src={logo9}
+                        alt="HK"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284363043-89f1bbc4-b919-4231-8b62-679785a2f20c.png"
-                        alt="Google"
+                        src={logo10}
+                        alt="Ifosys"
                         />
                         <img
-                        src="https://user-images.githubusercontent.com/78242022/284359560-9b7336a9-0dff-41c0-8f94-0a3ef06b679d.png"
-                        alt="Walmart"
+                        src={logo11}
+                        alt="Julius Baer"
+                        />
+                        <img
+                        src={logo12}
+                        alt="PIF"
+                        />
+                        <img
+                        src={logo13}
+                        alt="Saudia"
+                        />
+                        <img
+                        src={logo14}
+                        alt="Southwire"
+                        />
+                        <img
+                        src={logo15}
+                        alt="Taittinger"
+                        />
+                        <img
+                        src={logo16}
+                        alt="Tata Communications"
+                        />
+                        <img
+                        src={logo17}
+                        alt="Google Cloud"
+                        />
+                        <img
+                        src={logo18}
+                        alt="FIA"
                         />
                     </div>
                 </div>
